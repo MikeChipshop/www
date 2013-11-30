ed3f2581ec6c53aa21ee6caa972bd90e752259e0
MikeChipshop <mike@miniman-webdesign.co.uk>
2013-11-30 20:59:28 +0000

    Initial Commit

---
+++ single-investments.php
@@ -0,0 +1,34 @@
+<?php get_header(); ?>
+			<div class="container" >
+				<div  class="clearfix no_image grey" id="banner" ></div>
+			</div>
+
+			<!-- MAIN CONTENT AREA --> 
+			
+			<div class="grid_12 omega">
+			<div class="container clearfix content single_opp"> 
+					<div class="grid_10 push_1">
+						
+						
+						
+						<div class=""><img src="img/nac-logo.png"></div>
+						<div class="grid_2 omega">
+							<a href="#">Back</a>	
+						</div>
+						<div class="clearfix"></div>
+						<h5><?php the_title(); ?></h5>
+						<h6>Location - Norwich</h6>
+						<h6>Investment needed - &pound;50,000</h6>
+						<h6>Equity offered - 50%</h6>
+						
+						<div class="grid_12 omega"><p>dlskf slkdfjhsd flkj sdf ksdh sdkj fh sd hjf sf lskf slkdfjhsd flkj sdf ksdh sdkj fh sd hjf sflskf slkdfjhsd flkj sdf ksdh sdkj fh sd hjf sflskf slkdfjhsd flkj sdf ksdh sdkj fh sd hjf sf </p></div>
+
+						
+					</div>
+					<div class="grid_10 push_1">
+						<h6>Disclaimer</h6>
+						<p>Disclaimer will go here efkjh weoierfiuohdf ds uiui dsf hiusd dsu hfuer hfdh snc   d  oijoirh oi siw9i iwd9 ifoic i</p>
+					</div>
+			</div>
+			</div>
+<?php get_footer(); ?>
